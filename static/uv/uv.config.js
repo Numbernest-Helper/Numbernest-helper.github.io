b84c07b26a080f03cf09cf2f00b77e93478eb33c
Sebastian Solace <141687545+SebaztianSolace@users.noreply.github.com>
2025-02-04 10:45:53 -0800

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/tiw/',
-    bare: 'https://sysadmin.us.kg',
+    bare: 'https://homework.swat.lat',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
